6ef26a73e91ca6379eb4ff1339335813846710a8
mikmself <mikmself@gmail.com>
2024-11-23 08:31:32 +0700

    feat : add laravel import user

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -13,6 +14,29 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            ExcelImportAction::make()
+                ->color('primary') // Warna tombol Import
+                ->validateUsing([
+                    'name' => 'required|string',
+                    'email' => 'required|email',
+                    'password' => 'nullable|string',
+                    'unique_code' => 'nullable|string',
+                    'role' => 'required|string',
+                    'is_voted' => 'nullable|boolean',
+                    'session_id' => 'nullable|exists:session_votes,id',
+                ])
+                ->beforeImport(function (array $data) {
+                    // Mutasi data sebelum proses import
+                    $data['password'] = bcrypt($data['password'] ?? 'default_password'); // Hash password
+                    return $data;
+                })
+                ->afterImport(function (array $data) {
+                    \Filament\Notifications\Notification::make()
+                        ->title('Import Berhasil')
+                        ->body('Data berhasil diimpor. Total baris: ' . count($data))
+                        ->success()
+                        ->send();
+                }),
             Actions\CreateAction::make(),
         ];
     }
