5e1426137094486fc27a6321892114fc38416ba9
mikmself <mikmself@gmail.com>
2024-12-07 19:59:20 +0700

    feat : add user panitia

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,5 +25,18 @@ class UserSeeder extends Seeder
             'is_voted' => 'no',
             'session_id' => '1'
         ]);
+
+        $panitia = ["didi","aksana","wiqor","putri","hanan","gina","rafik","taufik","widya","kana","nurul","zhafira","sabita"];
+        foreach ($panitia as $name) {
+            User::create([
+                'name' => $name,
+                'email' => strtolower($name) . '@example.com',
+                'password' => Hash::make('password'),
+                'unique_code' => strtoupper(substr($name, 0, 2) . rand(1000, 9999)),
+                'role' => 'user',
+                'is_voted' => 'no',
+                'session_id' => '1'
+            ]);
+        }
     }
 }
